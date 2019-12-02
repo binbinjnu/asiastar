14d87bfe70532c2eca58fc30dfd36fa509d38ca6
daniel <daniel@163.com>
2019-12-02 15:13:53 +0700

    启动脚本修改

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+LOCAL_IP=`ifconfig -a|grep inet|grep -v 127.0.0.1|grep -v inet6|awk '{print $2}'|tr -d "addr:"`
+
 # 不能换行
-NODE_ID=1 ./_build/default/rel/asiastar/bin/asiastar start
+NODE_ID=1 IP=$LOCAL_IP ./_build/default/rel/asiastar/bin/asiastar start

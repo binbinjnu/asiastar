b6ea46040ad246a9982ab499d7f13183a00c8744
daniel <daniel@163.com>
2019-12-06 18:25:07 +0700

    脚本相关修改

--- src/util/util_list.erl
+++ src/util/util_list.erl
@@ -14,7 +14,15 @@
     shuffle/1,
     shuffle2/2,
 
-    batch/4
+    batch/4,
+
+    get_value/2,
+    get_value/3,
+    
+    index_of/2,
+    
+    set_list_nth/3
+
 ]).
 
 
@@ -50,3 +58,33 @@ batch(Func, Acc0, []=_Remain, _Max, AccData, _) ->
     Func(AccData, Acc0);
 batch(Func, Acc0, [H|T], Max, AccData, N) ->
     batch(Func, Acc0, T, Max, [H|AccData], N+1).
+
+%% 兼容List和maps的参数提取, 约等于proplist:get_value
+get_value(Key, Args) ->
+    get_value(Key, Args, undefined).
+
+
+get_value(Key, Maps, Default) when is_map(Maps) ->
+    maps:get(Key, Maps, Default);
+
+get_value(Key, List, Default) when is_list(List) ->
+    case lists:keyfind(Key, 1, List) of
+        {_, Val} -> Val;
+        false -> Default
+    end.
+
+
+%% 查找Item在列表List中的位置，如果没有则返回not_found
+index_of(Item, List) -> index_of(Item, List, 1).
+
+index_of(_, [], _)  -> not_found;
+index_of(Item, [Item | _], Index) -> Index;
+index_of(Item, [_ | T], Index) -> index_of(Item, T, Index + 1).
+
+
+%% 替掉列表中的第N项为Item
+set_list_nth(N, Item, [H|T]) when N > 1 ->
+    [H|set_list_nth(N-1, Item, T)];
+set_list_nth(_, Item, [_|T]) ->
+    [Item|T].
+
--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-ACTION=$1
-ID=$2
-IP=`ifconfig eth0|grep inet|grep -v 127.0.0.1|grep -v inet6|awk '{print $2}'|tr -d "addr:"`
-
-# 不能换行
-NODE_ID=$ID IP=$IP ./_build/default/rel/asiastar/bin/asiastar $ACTION
